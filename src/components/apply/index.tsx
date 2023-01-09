c37acc32fed5941e8631c2f581477972993f44ab
Yeong joon <bluehome8626@naver.com>
2023-01-09 16:19:05 +0900

    UPDATE: button animation 구현

--- src/components/apply/index.tsx
+++ src/components/apply/index.tsx
@@ -7,6 +7,13 @@ function NewApply() {
         <h1>Apply to NK</h1>
         <p>NK에서 다양한 경험을 해봐요</p>
       </TextContainer>
+      <ButtonContainer>
+        <AnimationContainer>
+          <p>NK 2기 지원하기</p>
+          <SlideBox id="slide" />
+        </AnimationContainer>
+        <CoverButton />
+      </ButtonContainer>
     </TotalWrapper>
   );
 }
@@ -16,35 +23,90 @@ export default NewApply;
 const TotalWrapper = styled.div`
   position: relative;
   z-index: 1;
-  width: 100%;
-  height: 1000px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
 const TextContainer = styled.div`
-  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 
   > h1 {
-    margin-top: 60px;
-    font-style: normal;
-    font-weight: 600;
-    font-size: 40px;
+    z-index: 4;
+    margin-top: 150px;
+    ${({ theme }) => theme.fontSizes.sectionTitle}
     color: ${({ theme }) => theme.colors.grayscale.gray20};
     text-align: center;
   }
 
   > p {
+    z-index: 4;
     font-style: normal;
     margin-top: 32px;
-    font-weight: 400;
-    font-size: 20px;
+    ${({ theme }) => theme.fontSizes.subTitle}
     color: ${({ theme }) => theme.colors.grayscale.gray20};
     text-align: center;
   }
+`;
+
+const ButtonContainer = styled.div`
+  position: relative;
+  margin-top: 200px;
+
+  :hover #slide {
+    left: 0px;
+    transition: 0.2s;
+  }
+`;
+
+const AnimationContainer = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  outline: none;
+  width: 360px;
+  height: 140px;
+  border: 1px solid ${({ theme }) => theme.colors.main.default};
+  border-radius: 16px;
+  background-color: ${({ theme }) => theme.colors.background.dark};
+  overflow: hidden;
+  z-index: 2;
+
+  > p {
+    z-index: 3;
+    ${({ theme }) => theme.fontSizes.title}
+    color: ${({ theme }) => theme.colors.grayscale.gray0};
+  }
+
+  :active #slide {
+    background-color: ${({ theme }) => theme.colors.background.light};
+    transition: 0s;
+  }
+`;
+
+const CoverButton = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 5px;
+  z-index: -1;
+  width: 352px;
+  height: 146px;
+  border: 1px solid ${({ theme }) => theme.colors.main.default};
+  border-radius: 16px;
+`;
+
+const SlideBox = styled.div`
+  width: 425px;
+  height: 600px;
+  background-color: ${({ theme }) => theme.colors.main.default};
+  position: absolute;
+  top: -320px;
+  left: -500px;
+  transform: rotate(40deg);
+  transition: 0.2s;
 `;
