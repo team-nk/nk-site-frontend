6910630db56309b3333b5c8d416c83353723e639
justinlee05 <justinlee05@naver.com>
2023-01-09 17:25:57 +0900

    UPDATE : 라우팅

--- src/route.tsx
+++ src/route.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header";
-import Background from "./components/background";
+import BackgroundMain from "./components/background/main";
 import TitleKR from "./components/main/titleKR";
 import styled from "styled-components";
 import TitleEN from "./components/main/titleEN";
@@ -10,27 +10,57 @@ import More from "./components/main/more";
 import Footer from "./components/footer";
 import MainPage from "./components/main";
 import ToTop from "./components/objects/totop";
+import Background from "./components/background";
+import TeamPage from "./components/team";
+import ProjectPage from "./components/project";
+import FAQPage from "./components/faq";
 
 function MainRouter() {
   return (
     <>
       <BrowserRouter>
         <Wrapper>
-          <Header />
-          <Background />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <MainPage />
-                </>
-              }
-            />
-          </Routes>
-          <ToTop />
+          <Background>
+            <Header />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <BackgroundMain />
+                    <MainPage />
+                  </>
+                }
+              />
+              <Route
+                path="/team"
+                element={
+                  <>
+                    <TeamPage />
+                  </>
+                }
+              />
+              <Route
+                path="/project"
+                element={
+                  <>
+                    <ProjectPage />
+                  </>
+                }
+              />
+              <Route
+                path="/faq"
+                element={
+                  <>
+                    <FAQPage />
+                  </>
+                }
+              />
+            </Routes>
+            <ToTop />
+            <Footer />
+          </Background>
         </Wrapper>
-        <Footer />
       </BrowserRouter>
     </>
   );
