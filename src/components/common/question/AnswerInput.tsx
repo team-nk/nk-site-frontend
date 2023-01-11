d64f38d5a3f1f8432849eac31e75f26401b9e732
Yeong joon <bluehome8626@naver.com>
2023-01-11 11:22:06 +0900

    ADD: apply에 사용할 질문 및 답 input 추가

---
+++ src/components/common/question/AnswerInput.tsx
@@ -0,0 +1,48 @@
+import { InputHTMLAttributes } from "react";
+import styled from "styled-components";
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  width?: number;
+  title: string;
+}
+
+function AnswerInput({ width, title, type = "text", ...props }: Props) {
+  return (
+    <Wrapper>
+      <QuestionText>{title}</QuestionText>
+      <AnswerInputText type={type} {...props} />
+    </Wrapper>
+  );
+}
+
+export default AnswerInput;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const QuestionText = styled.h1`
+  color: ${({ theme }) => theme.colors.grayscale.gray20};
+  ${({ theme }) => theme.fontSizes.subTitle}
+`;
+
+const AnswerInputText = styled.input`
+  width: 420px;
+  height: 56px;
+  padding: 16px 20px;
+  box-sizing: border-box;
+  outline: none;
+  border-top: 0;
+  border-right: 0;
+  border-left: 0;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.grayscale.gray20};
+  background-color: ${({ theme }) => theme.colors.background.dark};
+  color: ${({ theme }) => theme.colors.grayscale.gray20};
+  ${({ theme }) => theme.fontSizes.body1}
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.grayscale.gray60};
+    ${({ theme }) => theme.fontSizes.body1}
+  }
+`;
